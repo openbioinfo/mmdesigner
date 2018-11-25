2c9658b6208f1a59822fcbed6355543b5526918e
lusx <lusx@vienomics.com>
2018-11-25 22:19:40 +0800

    modify module3

--- methyprimer/filter_primer/CpG_filter.py
+++ methyprimer/filter_primer/CpG_filter.py
@@ -1,12 +1,21 @@
 import os 
-import re
 
 def CpG_filter(p3outfile):
 	
+	#PRIMER_LEFT_SEQUENCE就是正常参考序列的一样的部分，PRIMER_RIGHT_0_SEQUENCE是正常参考序列的反向互补序列
+	#而甲基化情况不同，先暂时只看正链的话，primer_left应该为甲基化参考序列的反向，primer_right应该为甲基化参考序列的反向互补
+	#现在的 left_primer换成甲基化参考序列之后需要做互补处理
 	file = open(p3outfile)
 	for line in file:
 		line = line.strip()
 		if line.startswith("SEQUENCE_ID"):
-			SEQUENCE_ID,locs = line.split("=")
+			nothing,name = line.split("=")
 			primer = dict()
-		if re.search('_SEQUENCE',line):
+		if 'PRIMER_LEFT' and '_SEQUENCE' in line:
+			nothing,sequence = line.split("=")
+			if 'CG' in sequence:
+				pass
+		if 'PRIMER_RIGHT' and '_SEQUENCE' in line:
+			nothing,sequence = line.split("+")
+			if 'CG' in sequnece:
+				pass
